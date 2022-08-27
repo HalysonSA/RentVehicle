4966624e7cc70384ff397b90b1147687a13eb84d
Halyson <92060880+HalysonSA@users.noreply.github.com>
2022-08-27 14:36:02 -0300

    Criacão do projeto / menu principal

---
+++ index.C
@@ -0,0 +1,27 @@
+///////////////////////////////////////////////
+// Autores: Halyson Santos de Araújo
+//          Pedro Henrique Ribeiro
+// Data: Agosto/2022.
+// Tema do Projeto: Locadora de Veículos 
+///////////////////////////////////////////////
+
+#include <stdio.h>
+
+void menu();
+
+int main(){
+    menu();
+    return 0;
+}
+
+void menu(){
+   
+    printf("=============== MENU PRINCIPAL ===============\n");
+    printf("======    1 - Controle de locacoes      ======\n");
+    printf("======    2 - Controle de clientes      ======\n");
+    printf("======    3 - Controle de funcionarios  ======\n");
+    printf("======    4 - Controle de veiculos      ======\n");
+    printf("======    5 - Controle do financeiro    ======\n");
+    printf("==============================================\n");
+}
+
