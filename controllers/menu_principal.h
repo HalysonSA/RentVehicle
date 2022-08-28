7216f2352260e5ffb6a6167deaa75b316165b9b6
Halyson <92060880+HalysonSA@users.noreply.github.com>
2022-08-28 10:03:54 -0300

    Versão final 0.1

--- controllers/menu_principal.h
+++ controllers/menu_principal.h
@@ -11,7 +11,6 @@ int menu_principal()
 {
 
     printf("=============== MENU PRINCIPAL ===============\n");
-    printf("======    0 - Encerrar Programa         ======\n");
     printf("======    1 - Controle de locacoes      ======\n");
     printf("======    2 - Controle de clientes      ======\n");
     printf("======    3 - Controle de funcionarios  ======\n");
