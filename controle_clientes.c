00db7ce7398e30b9b7490fe96e2133a8668e0254
Halyson <92060880+HalysonSA@users.noreply.github.com>
2022-09-10 12:00:05 -0300

    Entradas Controle de Clientes

--- controle_clientes.c
+++ controle_clientes.c
@@ -4,22 +4,36 @@
 
 void menu_cadastro_cliente()
 {
+    char nome[50], cpf[15], endereco[50], telefone[15];
+
     printf("=============================================\n");
     printf(" Digite o nome: \n");
+    scanf("%s", nome);
+    getchar();
     printf(" Digite o CPF: \n");
+    scanf("%s", cpf);
+    getchar();
     printf(" Digite o endereco: \n");
+    scanf("%s", endereco);
+    getchar();
     printf(" Digite o telefone: \n");
+    scanf("%s", telefone);
+    getchar();
     printf("=============================================\n");
+    printf(" Cadastro realizado com sucesso! \n");
 }
 
 void menu_relatorio_cliente()
 {
+    // Apenas um exemplo de como seria o relatorio
     printf("=============================================\n");
-    printf(" Relatorio de clientes cadastrados: \n");
+    printf("====  Relatorio de clientes cadastrados  ====\n");
+    printf("=============================================\n");
     printf("Nome: Joao da Silva\n");
     printf("CPF: 123.456.789-00\n");
     printf("Endereco: Rua das Flores, 123\n");
     printf("Telefone: (11) 1234-5678\n");
+    printf("=============================================\n");
     printf("Nome: Maria da Silva\n");
     printf("CPF: 987.654.321-00\n");
     printf("Endereco: Rua das Flores, 123\n");
@@ -27,23 +41,38 @@ void menu_relatorio_cliente()
     printf("=============================================\n");
 }
 
-void menu_editar_cliente()
+void menu_editar_cliente(char *cliente)
 {
+    char nome[50], cpf[15], endereco[50], telefone[15];
+
     printf("=============================================\n");
     printf(" Digite o nome: \n");
+    scanf("%s", nome);
+    getchar();
     printf(" Digite o CPF: \n");
+    scanf("%s", cpf);
+    getchar();
     printf(" Digite o endereco: \n");
+    scanf("%s", endereco);
+    getchar();
     printf(" Digite o telefone: \n");
+    scanf("%s", telefone);
+    getchar();
     printf("=============================================\n");
+    printf(" Cadastro editado com sucesso! \n");
 }
 
 void menu_remover_cliente()
 {
+    char cpf[15];
+
     printf("=============================================\n");
     printf(" Digite o CPF do cliente: \n");
+    scanf("%s", cpf);
+    getchar();
     printf("=============================================\n");
+    printf(" Cliente removido com sucesso! \n");
 }
-
 
 char menu_clientes()
 {
@@ -80,8 +109,16 @@ void controle_clientes()
             menu_relatorio_cliente();
             break;
         case '3':
-            menu_editar_cliente();
-            break;
+        {
+            char cpf[15];
+
+            printf("=============================================\n");
+            printf("Qual o CPF do cliente que deseja editar? \n");
+            scanf("%s", cpf);
+            getchar();
+            menu_editar_cliente(cpf);
+        }
+        break;
         case '4':
             menu_remover_cliente();
             break;
